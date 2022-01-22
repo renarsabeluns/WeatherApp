8464382bcfee0523b1165004fea2f9556479f924
Renārs <renars.abeluns@Inbox.lv>
2022-01-22 19:36:59 +0200

    Updated getIPdata, getWeather functions, now the main page is outputing weather data in JSON format

--- src/Controller/IpController.php
+++ src/Controller/IpController.php
@@ -11,41 +11,65 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class IpController extends AbstractController
 {
-   # public $ip = $request->getClientIp();
 
-    public function getIP(Request $request) 
+    private $ip;
+
+    private $region_name;
+    private $country_code;
+    public function getIP(Request $request)
     {
-        $ip = $request->getClientIp();
-        return $ip;
-    }    
+        $this->ip = '46.109.195.106';
+        #return $ip;
+    }
 
 
-
-    public function getWeather() 
+    public function getIPdata()
     {
-        $ch = HttpClient::create();
-        $response = $ch->request('GET', 'https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=26e29aa16ee3a3a8af761f4dd0410824');
-        $content = $response->getContent();
+        $client = HttpClient::create();
+
+        $response = $client->request('GET', 'http://api.ipstack.com/' . $this->ip . '?access_key=4ad0e02fbf2e1a55a886b65c9d4a7644',  ['headers' => [
+            'Accept' => 'application/json',
+        ]],);
+        $IpData = $response->toArray();
+        $this ->region_name = $IpData['region_name'];
+        $this ->country_code =$IpData['country_code'];
+        
+        return $IpData;
+    }
+
+
+    public function getWeather()
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://api.openweathermap.org/data/2.5/weather?q='.$this->region_name.','.$this->country_code.'&appid=26e29aa16ee3a3a8af761f4dd0410824', ['headers' => [
+            'Accept' => 'application/json',
+        ]],);
+        $WeatherData = $response->toArray();
         #curl_setopt($ch, CURLOPT_URL, 'api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=26e29aa16ee3a3a8af761f4dd0410824');
         #curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
         #curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        
+
         #$response = curl_exec($ch);
-        
+
         // If using JSON...
         #$data = json_decode($response);
         #$data2 = 
-        return $content;
-    }    
-
-    #[Route('/ip', name: 'ip')]
-    public function index(): Response
-    {
-        
-        return $this->render('ip/index.html.twig', [
-            'controller_name' => 'IpController',
-            'data' =>$this->getWeather()
-        ]);
+        return $WeatherData;
     }
 
+    #[Route('/ip', name: 'ip')]
+    public function index(Request $request): Response
+    {
+        $this->getIP($request);
+        $this->getIPdata();
+
+        return $this->render('ip/index.html.twig', [
+            'controller_name' => 'IpController',
+            'WeatherData' => $this->getWeather(),
+            'ip' => $this->ip,
+            'IpData' => $this->getIPdata(),
+            'country_code'=>$this->country_code
+
+        ]);
+    }
 }
