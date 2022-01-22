1a6ea5894db246875d195535bce0b4450939e221
Renārs <renars.abeluns@Inbox.lv>
2022-01-22 14:55:53 +0200

    Adjusted getWeather function for HttpClient

--- src/Controller/IpController.php
+++ src/Controller/IpController.php
@@ -7,25 +7,35 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\IPs;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpClient\HttpClient;
 
 class IpController extends AbstractController
 {
-  
+   # public $ip = $request->getClientIp();
+
+    public function getIP(Request $request) 
+    {
+        $ip = $request->getClientIp();
+        return $ip;
+    }    
+
+
 
     public function getWeather() 
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=26e29aa16ee3a3a8af761f4dd0410824');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $ch = HttpClient::create();
+        $response = $ch->request('GET', 'https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=26e29aa16ee3a3a8af761f4dd0410824');
+        $content = $response->getContent();
+        #curl_setopt($ch, CURLOPT_URL, 'api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=26e29aa16ee3a3a8af761f4dd0410824');
+        #curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
+        #curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         
-        $response = curl_exec($ch);
+        #$response = curl_exec($ch);
         
         // If using JSON...
-        $data = json_decode($response);
-
-        return $response;
+        #$data = json_decode($response);
+        #$data2 = 
+        return $content;
     }    
 
     #[Route('/ip', name: 'ip')]
