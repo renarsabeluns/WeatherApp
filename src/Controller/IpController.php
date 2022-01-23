ba3e1e9516919f729bc66e01bff2c224c5c8f394
Renārs <renars.abeluns@Inbox.lv>
2022-01-23 20:33:22 +0200

    Added caching principles

--- src/Controller/IpController.php
+++ src/Controller/IpController.php
@@ -7,7 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\IPs;
+use Symfony\Component\HttpClient\CachingHttpClient;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpKernel\HttpCache\Store;
 
 class IpController extends AbstractController
 {
@@ -16,7 +18,7 @@ class IpController extends AbstractController
 
     private $region_name;
     private $country_code;
-    public function getIP(Request $request)
+    public function getIP(Request $request,)
     {
         $this->ip = '46.109.195.106';
         #return $ip;
@@ -25,9 +27,11 @@ class IpController extends AbstractController
 
     public function getIPdata()
     {
+        $store = new Store('var\cache\dev\pools\app');
         $client = HttpClient::create();
+        $client = new CachingHttpClient($client, $store);
 
-        $response = $client->request('GET', 'http://api.ipstack.com/' . $this->ip . '?access_key=4ad0e02fbf2e1a55a886b65c9d4a7644',  ['headers' => [
+        $response = $client->request('GET', 'http://api.ipstack.com/' . $this->ip . '?access_key=7cdd5c912853213038180e6765e3edc1',  ['headers' => [
             'Accept' => 'application/json',
         ]],);
         $IpData = $response->toArray();
@@ -40,24 +44,17 @@ class IpController extends AbstractController
 
     public function getWeather()
     {
+        $store = new Store('var\cache\dev\pools\app');
         $client = HttpClient::create();
+        $client = new CachingHttpClient($client, $store);
         $response = $client->request('GET', 'https://api.openweathermap.org/data/2.5/weather?q='.$this->region_name.','.$this->country_code.'&appid=26e29aa16ee3a3a8af761f4dd0410824', ['headers' => [
             'Accept' => 'application/json',
         ]],);
         $WeatherData = $response->toArray();
-        #curl_setopt($ch, CURLOPT_URL, 'api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=26e29aa16ee3a3a8af761f4dd0410824');
-        #curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
-        #curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        #$response = curl_exec($ch);
-
-        // If using JSON...
-        #$data = json_decode($response);
-        #$data2 = 
         return $WeatherData;
     }
 
-    #[Route('/ip', name: 'ip')]
+    #[Route('/', name: 'ip')]
     public function index(Request $request): Response
     {
         $this->getIP($request);
